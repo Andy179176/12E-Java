3454c9953ad45c9306fa614739bb88f27bb41d8b
stingrey <maria.morenko06@gmail.com>
2020-08-18 18:02:19 +0200

    homework write method for revese String and check with condition String

--- 20200817_interfaceUsingDemo/src/com/company/Main.java
+++ 20200817_interfaceUsingDemo/src/com/company/Main.java
@@ -3,15 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        // etirw
+        String[] array = {"apple, pear, pineapple, 12345"};
+        StringArrayHandler stringArrayHandler1 = new StringArrayHandler(array);
+
+        Condition condition1 = new CheckStringCondition();
+        Action action1 = new ReverseStringAction();
+        //stringArrayHandler1.stringsHandle(action1.doAction(), condition1.test());
     }
-
-
-
-    /*
-    1. перебрать массив
-    2. выбрать нужные (условие)
-    3. реверс (действие)
-     */
 }
--- 20200817_interfaceUsingDemo/src/com/company/ReverseStringAction.java
+++ 20200817_interfaceUsingDemo/src/com/company/ReverseStringAction.java
@@ -3,7 +3,12 @@ package com.company;
 public class ReverseStringAction implements Action {
 
     @Override
-    public String doAction(String str) {
-        return null;
+    public String doAction(String string) {
+        char[] chars = string.toCharArray();
+        String reversedString = "";
+        for (int i = chars.length - 1; i >= 0; i--) {
+            reversedString += chars[i];
+        }
+        return reversedString;
     }
 }
