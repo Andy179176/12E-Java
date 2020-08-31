efbe1b2edac724ff5fc7d164e2538a21bc4f213a
stingrey <maria.morenko06@gmail.com>
2020-08-31 12:07:47 +0200

    Write the program that create and displays the list of String (name and email)

---
+++ homeworlPersonListAndAddress/src/com/homework/Address.java
@@ -0,0 +1,28 @@
+package com.homework;
+
+/**
+ * JavaAdvanced
+ * 30.08.20 20: 23
+ */
+public class Address {
+    private String type;
+    private String name;
+
+    public Address(String type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
---
+++ homeworlPersonListAndAddress/src/com/homework/AddressTypesEmail.java
@@ -0,0 +1,24 @@
+package com.homework;
+
+import java.util.function.Predicate;
+
+/**
+ * JavaAdvanced
+ * 30.08.20 21: 29
+ */
+public class AddressTypesEmail implements Predicate<Person> {
+    // если у person нет фамилии, то добавлять в список
+    @Override
+    public boolean test(Person person) {
+        if (person.fingEmailType() == null || person.fingEmailType().equals("")) {
+            return false;
+        }
+        for (Address address : person.getListAddress()) {
+            if (address.getType().equals("email") && person.getSurname() != "" && person.getSurname() != null) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
