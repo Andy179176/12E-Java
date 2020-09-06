1946274f8f1b29de8f5e9e414d66c3105544111b
stingrey <maria.morenko06@gmail.com>
2020-09-06 21:30:43 +0200

    Created a list of Person that does not have a billing address

---
+++ PersonAndAdressWithoutBilling/src/homework020PersonAndAdressWithoutBilling/Address.java
@@ -0,0 +1,26 @@
+package homework020PersonAndAdressWithoutBilling;
+
+public class Address {
+    private String address;
+    private AddressType type;
+
+    public Address(String address, AddressType type) {
+        this.address = address;
+        this.type = type;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public AddressType getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return address;
+    }
+
+
+}
